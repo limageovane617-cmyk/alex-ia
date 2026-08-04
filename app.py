d02433cc6c729f7763bdb3a06b466531ce51dd18
limageovane617-cmyk <limageovane617@gmail.com>
2026-08-04 17:12:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from google import genai
 st.set_page_config(page_title="Alex IA", page_icon="🤖")
 
 st.title("🤖 Alex IA")
-st.write("Sua IA usando Google Gemini")
+st.write("Sua IA usando o Google Gemini")
 
 api_key = st.text_input(
     "Digite sua chave da API Gemini:",
@@ -19,7 +19,7 @@ if api_key:
 
         if st.button("Enviar") and pergunta:
             resposta = client.models.generate_content(
-                model="gemini-2.5-flash-lite",
+                model="gemini-2.5-flash",
                 contents=pergunta
             )
 
